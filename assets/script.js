60afa0f5afc862417169c6c39d9bdefbd2d236c6
Hope <johnhope354@gmail.com>
2021-05-22 14:23:30 -0500

    added local storage

--- assets/script.js
+++ assets/script.js
@@ -2,12 +2,12 @@
 let zipInput = document.querySelector("#zipInput");
 let getWeatherBtn = document.querySelector("#getWeatherBtn")
 let results = document.querySelector('.results')
+let previousSearches = document.querySelector(".previousSearches")
+let recentSearches = document.querySelector("#searches")
+let previousSearchTitles = document.querySelector(".previousSearchTitles")
 let currentTemp = document.querySelector('#currentTemp');
 let currentWind = document.querySelector('#currentWind')
 let currentHumidity = document.querySelector('#currentHumidity')
-let tomTemp = document.querySelector('#currentDay1')
-let previousSearches = document.querySelector(".previousSearches")
-let recentSearches = document.querySelector("#searches")
 let forecastZeroTemp = document.querySelector("#forecastZeroTemp")
 let forecastOneTemp = document.querySelector("#forecastOneTemp")
 let forecastTwoTemp = document.querySelector("#forecastTwoTemp")
@@ -23,11 +23,6 @@ let forecastOneHumidity = document.querySelector("#forecastOneHumidity")
 let forecastTwoHumidity = document.querySelector("#forecastTwoHumidity")
 let forecastThreeHumidity = document.querySelector("#forecastThreeHumidity")
 let forecastFourHumidity = document.querySelector("#forecastFourHumidity")
-let forecastZeroUv = document.querySelector("#forecastZeroUv")
-let forecastOneUv = document.querySelector("#forecastOneUv")
-let forecastTwoUv = document.querySelector("#forecastTwoUv")
-let forecastThreeUv = document.querySelector("#forecastThreeUv")
-let forecastFourUv = document.querySelector("#forecastFourUv")
 let forecastZeroDate = document.querySelector("#forecastZeroDate")
 let forecastOneDate = document.querySelector("#forecastOneDate")
 let forecastTwoDate = document.querySelector("#forecastTwoDate")
@@ -38,6 +33,7 @@ let dateTime = luxon.DateTime;
 let dt = dateTime.now();
 let today = dateTime.local().toLocaleString(dateTime.DATE_HUGE);
 let date = document.querySelector("#currentDay");
+
 date.textContent = today;
 
 var zipSubmitHandler = function (event) {
@@ -45,43 +41,53 @@ var zipSubmitHandler = function (event) {
     var zipCode = zipInput.value;
 
     if (zipCode) {
-        getWeather(zipCode);
-        results.style.display = "block";
-        previousSearches.style.display = 'block';
+        getWeather(zipCode, event);
         zipInput.value = '';
     } else {
         alert('Please enter a valid zip');
     }
 
 }
-var entry = JSON.parse(localStorage.getItem("entry"))
-let getWeather = function (zipCode) {
+let getWeather = function (zipCode, event) {
     let requestUrl = 'https://api.openweathermap.org/data/2.5/weather?zip=' + zipCode + ',us&units=imperial&appid=aa549204eace54c9a130ca02cb68afc8'
     fetch(requestUrl)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-            console.log('Fetch Response \n-----------');
-            console.log(data);
-            let latitude = data.coord.lat
-            let longitude = data.coord.lon
-            city.textContent = data.name
-            cityName = data.name
-            currentTemp.textContent = "Temp: " + data.main.temp;
-            currentWind.textContent = "Wind: " + data.wind.speed + " MPH";
-            currentHumidity.textContent = "Humidity: " + data.main.humidity + "%";
-            let searchBtn = document.createElement('button')
-            let newContent = document.createTextNode(cityName)
-            searchBtn.appendChild(newContent)
-            document.body.insertBefore(searchBtn,results);
-            /*entry.push({
-                "city" : data.name,
-                "zip" : zipCode
-            });
-            localStorage.setItem("entry", JSON.stringify(entry))
-            console.log(localStorage)*/
-            getForecast(latitude, longitude);
+            if (data.cod === "404") {
+                alert('Please enter a valid zip');
+            }
+            else {
+                console.log('Fetch Response \n-----------');
+                console.log(data);
+                let latitude = data.coord.lat
+                let longitude = data.coord.lon
+                city.textContent = data.name
+                cityName = data.name
+                currentTemp.textContent = "Temp: " + data.main.temp;
+                currentWind.textContent = "Wind: " + data.wind.speed + " MPH";
+                currentHumidity.textContent = "Humidity: " + data.main.humidity + "%";
+
+                if (/*if zipcode has already been entered*//*if */event.target.id === "getWeatherBtn") {
+                    let searchBtn = document.createElement('button')
+                    let newContent = document.createTextNode(cityName)
+                    searchBtn.appendChild(newContent)
+                    searchBtn.setAttribute("data",zipCode);
+                    searchBtn.setAttribute("class","searchClass");
+                    //document.body.insertAfter(searchBtn, previousSearchTitles);
+                    previousSearches.append(searchBtn);
+                    var data = JSON.parse(localStorage.getItem("entry")) || [];
+                    var dataEntry = {
+                        city : data.name,
+                        zip : zipCode
+                    };
+                    data.push(dataEntry)
+                    localStorage.setItem("entry", JSON.stringify(data))
+                    console.log(localStorage)
+                }
+                getForecast(latitude, longitude);
+            }
         })
 }
 
@@ -101,6 +107,16 @@ function getForecast(latitude, longitude) {
             forecastThreeDate.textContent = dateTime.now().plus({ days: 4 }).toLocaleString(dateTime.DATE_HUGE);
             forecastFourDate.textContent = dateTime.now().plus({ days: 5 }).toLocaleString(dateTime.DATE_HUGE);
             currentUv.textContent = "UV Index: " + data.current.uvi;
+            if (data.current.uvi < 2) {
+                currentUv.style.backgroundColor = "green";
+            }
+            else if (2 <= data.current.uvi <= 5) {
+                currentUv.style.backgroundColor = "yellow";
+            }
+            else {
+                currentUv.style.backgroundColor = "red";
+                currentUv.style.color = "white"
+            }
             forecastZeroTemp.textContent = "Temp: " + data.daily[1].temp.max
             forecastOneTemp.textContent = "Temp: " + data.daily[2].temp.max
             forecastTwoTemp.textContent = "Temp: " + data.daily[3].temp.max
@@ -116,13 +132,23 @@ function getForecast(latitude, longitude) {
             forecastTwoHumidity.textContent = "Humidity: " + data.daily[3].humidity + "%";
             forecastThreeHumidity.textContent = "Humidity: " + data.daily[4].humidity + "%";
             forecastFourHumidity.textContent = "Humidity: " + data.daily[5].humidity + "%";
-            forecastZeroUv.textContent = "UV Index: " + data.daily[1].uvi
-            forecastOneUv.textContent = "UV Index: " + data.daily[2].uvi
-            forecastTwoUv.textContent = "UV Index: " + data.daily[3].uvi
-            forecastThreeUv.textContent = "UV Index: " + data.daily[4].uvi
-            forecastFourUv.textContent = "UV Index: " + data.daily[5].uvi
         })
-    
+    results.style.display = "block";
+    previousSearches.style.display = 'block';
+
 }
 
+//have each searchBtn added to local storage
+
 getWeatherBtn.addEventListener("click", zipSubmitHandler)
+
+previousSearches.addEventListener("click", (event) => {
+    console.log("******",event);
+    //check if the thing we clicked on is the city btn
+    if (event.target.className.indexOf("searchClass") > -1) {
+        //get the zip code
+        var getZip = event.target.getAttribute("data");
+        //run get weather
+        getWeather(getZip, event)
+    }
+})
